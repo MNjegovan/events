f4303721f4b5ffcc23f9cd7c5ae9eea6beb1857e
MNjegovan <mnjegovan99@gmail.com>
2017-03-16 06:16:53 -0200

    Add files via upload

---
+++ batomn.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Ucenik on 16.3.2017..
+ */
+/*var mojGumbVar = document.getElementById("mojGumb");
+mojGumbVar.onclick = function () {
+    alert("tekst")
+};*/
+/*document.getElementById("down").addEventListener("keydown",myCall);
+function myCall(){
+    document.getElementById("down").innerHTML
+    document.getElementById("pe").innerHTML = "pe qu"
+}*/
+function funk1(){
+    alert("dolje")
+}
+function funk2() {
+    alert("gore")
+}
+function funk3() {
+    alert("pritisak")
+}
+function funk4(){
+    alert("debilu slomio si svemir")
+}
+function funk5(){
+    alert("loadalo se")
+}
